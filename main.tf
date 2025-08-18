1cc38d3f4432fe5d8b1d5fc998d093cbd9304c68
miakuricATU <X00218710@mytudublin.ie>
2025-08-18 16:01:34 +0100

    chaning tf files

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.name_prefix}-rg"
+  location = var.location
+}
+
+resource "random_integer" "rand" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.name_prefix}acr${random_integer.rand.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.name_prefix}-aks"
   location            = azurerm_resource_group.rg.location
@@ -6,10 +24,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "nodepool1"
-    node_count = 1            # was 2
-    vm_size    = "Standard_B4ms"
+    node_count = 2             # reduced to fit vCPU quota
+    vm_size = "Standard_B2ms"
   }
 
   identity { type = "SystemAssigned" }
   role_based_access_control_enabled = true
+}
+
+resource "azurerm_role_assignment" "aks_pull_acr" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
 }
